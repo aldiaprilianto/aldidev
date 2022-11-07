ad1288ac69e7c527266b2468f7c089c417f3a598
aldi aprilianto <aldiaprilianto23@gmail.com>
2022-11-07 11:13:45 +0700

    new aldidev

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/aldiaprilianto/aldidev
+
+go 1.19
---
+++ greet.go
@@ -0,0 +1,37 @@
+package greet
+
+import (
+	"fmt"
+)
+
+type Person struct {
+	Id    int
+	Name  string
+	Hobby string
+}
+
+func (p Person) GetId() int {
+	return p.Id
+}
+
+func (p *Person) SetId(id int) {
+	p.Id = id
+}
+func (p Person) GetName() string {
+	return p.Name
+}
+
+func (p *Person) SetName(name string) {
+	p.Name = name
+}
+func (p Person) GetHobby() string {
+	return p.Hobby
+}
+
+func (p *Person) SetHobby(hobby string) {
+	p.Hobby = hobby
+}
+
+func (p Person) ToString() string {
+	return fmt.Sprintf("Hallo, nama saya %s dan hobby saya adalah %s.", p.GetName(), p.GetHobby())
+}
